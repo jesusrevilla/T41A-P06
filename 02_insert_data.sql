4dbb495460271101213e24a8042861bfbeded550
aldogg10 <179862@upslp.edu.mx>
2025-10-06 19:31:48 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,1 +1,3 @@
-
+INSERT INTO clientes VALUES (1, 'Cristiano Ronaldo', '2025-07-18', true, 700.25);
+INSERT INTO clientes VALUES (2, 'John Cena', '2025-10-06', true, 300.25);
+INSERT INTO clientes VALUES (3, 'Leo Messi', '2025-02-22', false, 10.25);

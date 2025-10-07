829531dd3664d928e5f7b7b574fb7225bab4265e
Fano-Rdz <178584@upslp.edu.mx>
2025-10-06 19:33:52 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,1 +1,5 @@
-
+INSERT INTO clientes (nombre, saldo, activo) VALUES
+('Ana Pérez', 1500.75, TRUE),
+('Luis Gómez', 500.00, FALSE),
+('María López', 2300.50, TRUE),
+('Carlos Sánchez', 750.00, TRUE),

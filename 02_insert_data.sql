eb8fc1fa83dfcb0cfeb7ff85e154a151ad2e32eb
tacodepastor25 <125671857+tacodepastor25@users.noreply.github.com>
2025-10-06 19:31:34 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,1 +1,5 @@
-
+INSERT INTO clientes (nombre, fecha_registro, activo, saldo)
+VALUES 
+('Josue Pineda', '2025-07-03', TRUE, 1400.00),
+('Italia Robles', '2025-11-22', TRUE, 2600.50),
+('Manuel Oliva', '2023-09-25', FALSE, 800.00);

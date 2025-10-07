f6269967f1df78d99f3ab0cd05695b775f052e7e
m00n07 <174653@upslp.edu.mx>
2025-10-06 19:28:15 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,1 +1,9 @@
+-- insertar datos simulando una petición de cliente
+INSERT INTO clientes (nombre, fecha_registro, activo, saldo) VALUES
+('Ana Torres', '2025-08-22', TRUE, 230.00);
 
+INSERT INTO clientes (nombre, fecha_registro, activo, saldo) VALUES
+('Hugo Lopez', '2025-10-02', TRUE, 961.00);
+
+INSERT INTO clientes (nombre, fecha_registro, activo, saldo) VALUES
+('Oscar Gacía', '2025-09-15', FALSE, 498.00);

4bbe6f0f60d7b1dcb856c6432691f147d77d237c
Palau003 <178561@upslp.edu.mx>
2025-10-06 19:31:33 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,1 +1,5 @@
-
+INSERT INTO clientes (nombre, fecha_registro, activo, saldo)
+VALUES 
+('Juan Pérez', '2025-10-01', TRUE, 1500.75),
+('María García', '2025-09-20', TRUE, 2450.50),
+('Luis Torres', '2025-08-15', FALSE, 0.00);

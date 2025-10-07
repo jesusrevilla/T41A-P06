6b2f7baec5026ac7d4a676435f852b62d17fcadc
SayraIbarra <130393897+SayraIbarra@users.noreply.github.com>
2025-10-06 19:35:40 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,1 +1,8 @@
-
+-- Crear tabla clientes con los distintos tipos de datos
+CREATE TABLE clientes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    fecha_registro DATE,
+    activo BOOLEAN,
+    saldo DECIMAL(10, 2)
+);

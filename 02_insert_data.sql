558f6b7a1178174a7483ae42d8984445a55b5830
SebasMonreal11 <125666778+SebasMonreal11@users.noreply.github.com>
2025-10-06 19:30:29 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,1 +1,6 @@
+INSERT INTO clientes (nombre, fecha_registro, activo, saldo)
+VALUES 
+    ('Ana Torres', '2025-10-01', TRUE, 1500.75),
+    ('Luis Gómez', '2025-10-03', TRUE, 230.00),
+    ('Carlos Ruiz', '2025-09-28', FALSE, 0.00);
 

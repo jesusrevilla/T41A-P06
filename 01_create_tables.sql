8e804c0056fc4ebddc5a4cb3b79f507465829983
Cristphertorres21 <cristophertorresdza@gmail.com>
2025-10-06 19:28:55 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,7 +1,9 @@
+DROP TABLE IF EXISTS clientes;
+
 CREATE TABLE clientes (
-  id SERIAL PRIMARY KEY,                    
-  nombre VARCHAR(100) NOT NULL,              
-  fecha_registro DATE NOT NULL,              
-  activo BOOLEAN NOT NULL,                   
-  saldo NUMERIC(10, 2) NOT NULL              
+  id SERIAL PRIMARY KEY,                     -- entero autoincremental
+  nombre VARCHAR(100) NOT NULL,              -- tipo character varying
+  fecha_registro DATE NOT NULL,              -- tipo fecha
+  activo BOOLEAN NOT NULL,                   -- valor verdadero/falso
+  saldo NUMERIC(10, 2) NOT NULL              -- tipo numérico con precisión
 );

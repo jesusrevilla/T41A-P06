d713a62e158a5650e2471ef69f905b8311eeba73
EdgarErh <160251219+EdgarErh@users.noreply.github.com>
2025-10-06 19:33:10 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,1 +1,7 @@
-
+CREATE TABLE clientes (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    fecha_registro DATE DEFAULT CURRENT_DATE,
+    activo BOOLEAN DEFAULT TRUE,
+    saldo NUMERIC(10, 2)
+);

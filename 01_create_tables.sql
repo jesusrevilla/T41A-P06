38f1347a8b9654d5f6bd61cebc926767dd39e525
Cruz <183213@upslp.edu.mx>
2025-10-06 21:34:27 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,1 +1,7 @@
-
+CREATE TABLE cliente(
+id SERIAL PRIMARY KEY,               -- tipo entero autoincremental
+nombre VARCHAR(100),           -- cadena de texto, 100 caracteres
+fecha_registro TIMESTAMP,   -- fecha
+activo BOOLEAN,          -- valor verdadero/falso
+saldo NUMERIC(10,2)            -- Número decinal con presición (10, 2)
+);

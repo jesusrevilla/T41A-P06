9664e0acdd957b65abc60b84546db0ae6d2f236f
ItsMusa17 <160455957+ItsMusa17@users.noreply.github.com>
2025-10-07 09:19:43 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,1 +1,11 @@
-
+INSERT INTO clientes (codigo_del_cliente, nombre_del_cliente)
+VALUES ('C100', 'Cliente Prueba');
+ 
+INSERT INTO articulo (codigo_del_articulo, nombre_del_articulo, precio_unitario)
+VALUES ('A100', 'Articulo Prueba', 50.00);
+ 
+INSERT INTO factura (sucursal, numero_de_factura, fecha_de_la_factura, forma_de_pago_factura, codigo_del_cliente, total_de_la_factura)
+VALUES ('S1', 1, CURRENT_DATE, 'Efectivo', 'C100', 100.00);
+ 
+INSERT INTO detalle_de_factura (sucursal, numero_de_factura, codigo_de_articulo, cantidad_del_articulo, precio_unitario_del_articulo, subtotal_del_articulo)
+VALUES ('S1', 1, 'A100', 2, 50.00, 100.00);

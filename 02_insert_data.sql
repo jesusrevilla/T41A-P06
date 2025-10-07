8cd4d07d3266361b72c30dd8942bf5fd0a182eba
David Vidales <vidalesrmzdavid@gmail.com>
2025-10-06 19:44:41 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,4 +1,4 @@
-NSERT INTO clientes (nombre,fecha_registro,activo,saldo) VALUES
+INSERT INTO clientes (nombre,fecha_registro,activo,saldo) VALUES
 ('Angel','06/10/2025',TRUE,123144.23),
   ('yAamin','02/10/2025',TRUE,13144.23),
 ('David','02/10/2025',FALSE,1231234.32);

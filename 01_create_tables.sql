45f37b52645b16c7ece471a1f5d6e84666a508d9
Bemused5 <104637264+Bemused5@users.noreply.github.com>
2025-10-06 19:21:03 -0600

    feature/177406

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,1 +1,7 @@
-
+CREATE TABLE clientes (
+  id               SERIAL PRIMARY KEY,     
+  nombre           VARCHAR(100) NOT NULL,  
+  fecha_registro   DATE NOT NULL DEFAULT CURRENT_DATE,
+  activo           BOOLEAN NOT NULL DEFAULT TRUE,
+  saldo            NUMERIC(10,2) NOT NULL DEFAULT 0.00
+);

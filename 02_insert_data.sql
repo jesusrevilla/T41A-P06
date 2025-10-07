4cde29f80451a042e04c898c82e2e9419364ffe6
Juan Pablo Csnv <175329@upslp.edu.mx>
2025-10-06 19:42:05 -0600

    datos-175329

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,1 +1,5 @@
+INSERT INTO clientes (id, nombre, fecha_registro, activo, saldo) VALUES 
+  (1,'Juan CSNV', '2025-10-06', TRUE, 250),
+  (2,'AAAAA', '2025-09-16', TRUE, 550.11),
+  (3,'BBBBB', '2025-08-11', FALSE, 133.84);
 

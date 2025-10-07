aa7a6f53b3fa3e089b201f1565ebc6e6f9162412
menaxixi <179800@upslp.edu.mx>
2025-10-06 19:34:37 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,1 +1,4 @@
-
+INSERT INTO clientes (nombre, fecha_registro, activo, saldo) VALUES 
+  ('Paty Martínez', '2025-10-01', TRUE, 1500.50),
+  ('Daniel Sánchez', '2025-10-02', TRUE, 2300.75),
+  ('Rafael Sánchez', '2025-10-03', FALSE, 0.00);

ca542de83378f463fe86a13b4d1cadc8272fa8e1
tacodepastor25 <125671857+tacodepastor25@users.noreply.github.com>
2025-10-06 19:30:08 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,1 +1,5 @@
-
+INSERT INTO clientes (nombre, fecha_registro, activo, saldo)
+VALUES 
+('Juan Pérez', '2025-10-01', TRUE, 1500.75),
+('María López', '2025-09-28', TRUE, 2300.50),
+('Carlos Ruiz', '2025-09-15', FALSE, 500.00);

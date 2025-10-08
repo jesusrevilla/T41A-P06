5f9df876be876b2616bf297afe167d8a4084d885
Juan Pablo Csnv <175329@upslp.edu.mx>
2025-10-07 19:14:06 -0600

    tablas-175329

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -3,5 +3,6 @@ CREATE TABLE clientes (
   nombre VARCHAR(50) NOT NULL,
   fecha_registro DATE NOT NULL,
   activo BOOLEAN NOT NULL,
-  saldo FLOAT(10,2) NOT NULL
+  saldo NUMERIC(10,2) NOT NULL,
+  PRIMARY KEY (id)
 );
